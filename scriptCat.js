245523b4c8f09357a7421f91c05cf2c74997aabb
Vittorio <vittofv01@gmail.com>
2023-02-15 17:32:16 +0100

    .

--- scriptCat.js
+++ scriptCat.js
@@ -3,3 +3,9 @@ document.querySelector("#musica a h2").innerHTML = "MUSICA"
 
 document.querySelector("#film a").setAttribute("href","form.html#11")
 document.querySelector("#film a h2").innerHTML = "FILM"
+
+document.querySelector("#libri a").setAttribute("href","form.html#10")
+document.querySelector("#libri a h2").innerHTML = "LIBRI"
+
+document.querySelector("#videogame a").setAttribute("href","form.html#15")
+document.querySelector("#videogame a h2").innerHTML = "VIDEO GAMES"
